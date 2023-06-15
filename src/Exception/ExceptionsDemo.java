3fa0179877dfe87115438ab4a31a4e16f9072c3e
Gokul1909 <gokul@terzocloud.com>
2023-06-15 12:19:28 +0530

    Exception implementation

--- src/Exception/ExceptionsDemo.java
+++ src/Exception/ExceptionsDemo.java
@@ -1,4 +1,36 @@
 package Exception;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class ExceptionsDemo {
+    public static void show() {
+        //Handling Checked exceptions
+        FileReader reader = null;
+        try {
+            reader = new FileReader("file1.txt");
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("The file doesn't exist");
+        }
+        finally {  //To make the close() get executed in all cases
+            try {
+                reader.close();
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+            //Handling Unchecked exceptions
+            finally {
+                try {
+                    int b = 0;
+                    int a = 10 / b;
+                } catch (ArithmeticException e) {
+                    System.out.println("Divide by zero error");
+                }
+            }
+
+        }
+
+    }
 }
